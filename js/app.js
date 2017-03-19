d6bc4b896e937aca0d421846a2508530850cc6eb
Lee Moser <lee.moser@uky.edu>
2017-03-18 20:20:17 -0400

    rough functionality, basic layout, and data loading

--- js/app.js
+++ js/app.js
@@ -4,16 +4,34 @@
     L.mapbox.accessToken = 'pk.eyJ1IjoibGptb3NlcjgzIiwiYSI6ImNpemNyNzhjODFuOXoycHFvbTU3Y25qMnIifQ.AT3F-314hvPwaNr4WxIgDw';
 
     // create the Leaflet map using mapbox.light tiles
+    // var map = L.map('map');
+
     var map = L.mapbox.map('map', 'mapbox.light', {
         zoomSnap: .1,
         center: [45, -106],
         zoom: 4,
         minZoom: 4,
         maxZoom: 9,
-        //maxBounds: L.latLngBounds([-6.22, 27.72], [5.76, 47.83])
     });
+    $.getJSON("data/refugees_2008_2016_4326.json", function (data) {
+            var dataLayer = L.geoJson(data, {
+                style: function (feature) {
+                    return {
+                        color: '#dddddd',
+                        weight: 1,
+                        fillOpacity: .55,
+                        fillColor: '#1f78b4'
+                    };
 
-    // create Leaflet control for the slider
+                }
+
+
+            }).addTo(map);
+            console.log(dataLayer);
+        drawLegend(dataLayer);
+
+        })
+        // create Leaflet control for the slider
     var sliderControl = L.control({
         position: 'bottomleft'
     });
@@ -49,75 +67,30 @@
 
     yearDisplay.addTo(map);
 
-    // load CSV data
-    omnivore.csv('data/kenya_education_2014.csv')
-        .on('ready', function (e) {
-            drawMap(e.target.toGeoJSON());
-            drawLegend(e.target.toGeoJSON());
-        })
-        .on('error', function (e) {
-            console.log(e.error[0].message);
-        });
 
-    var options = {
-        pointToLayer: function (feature, ll) {
-            return L.circleMarker(ll, {
-                opacity: 1,
-                weight: 2,
-                fillOpacity: 0,
-            })
-        }
-    }
+    function drawMap(data) {
 
-    function drawMap() {
-        //creates a seperate layer for girls and boys
-        //var girlsLayer = L.geoJson(data, options).addTo(map);
-        //var boysLayer = L.geoJson(data, options).addTo(map);
+
         //creates a var named currentYear to set initial value of year identifier div upon webpage load
         var currentYear = 2016
             //JQuery method to update html of selected div
         $('#year').html("Year:" + " " + currentYear);
-        //defines the color of the girls layer
-        girlsLayer.setStyle({
-            color: '#D96D02',
-        });
-        //defines the color of the boys layer
-        boysLayer.setStyle({
-            color: '#6E77B0',
-        });
-        //call to the function resizeCircle passing girlsLayer, boysLayer, and 1 as arguments, which calculates the proportional size of the circles on the map//
-        resizeCircles(girlsLayer, boysLayer, 1);
+
+
         //call to retrieveInfo function that is used to build info window
-        retrieveInfo(boysLayer, currentYear);
+        retrieveInfo(dataLayer, currentYear);
         //call to sequenceUI function that established slider interaction
-        sequenceUI(girlsLayer, boysLayer);
-        //sets the extent of the map to the extent of the girlsLayer
-        map.fitBounds(girlsLayer.getBounds());
-    }
-    //a function defined to calculate the radius of the L.circle markers that are a proportional representation of the enrollment count by gender
-    function calcRadius(val) {
-        var radius = Math.sqrt(val / Math.PI);
-        return radius * 0.5
-    }
-    //a function that resizes the circle markers on the map
-    function resizeCircles(girlsLayer, boysLayer, currentYear) {
-        girlsLayer.eachLayer(function (layer) {
-            var radius = calcRadius(Number(layer.feature.properties['G' + currentYear]));
-            layer.setRadius(radius);
-        });
-        boysLayer.eachLayer(function (layer) {
-            var radius = calcRadius(Number(layer.feature.properties['B' + currentYear]));
-            layer.setRadius(radius);
-        });
+        sequenceUI(dataLayer);
+
 
     }
+
     //adds an event listener that updates the value of currentYear each time the user interacts with the slider. This function also uses a JQuery method to update the content of the year label div.//
     function sequenceUI() {
         $('.slider').on('input change', function () {
             var currentYear = $(this).val();
             $('#year').html("Year:" + " " + currentYear);
-            resizeCircles(girlsLayer, boysLayer, currentYear);
-            retrieveInfo(boysLayer, currentYear);
+            retrieveInfo(dataLayer, currentYear);
         });
 
     }
@@ -142,159 +115,8 @@
 
             }
             // add the control to the map
-            legend.addTo(map);
-
-            
-            //Create an empty array named dataValues.
-        var dataValues = [];
-        //Use the map method to iterate through the data.features.school.properties.year enumerable enrollment values and push all numeric values to the array to sort for maximum of the enrollment rates.//
-        data.features.map(function (school) {
-
-            for (var year in school.properties) {
-
-                var attribute = school.properties[year];
-                //if statement restricts values pushed to array to only numeric values.
-                if (Number(attribute)) {
-
-                    dataValues.push(attribute);
-                }
-
-            }
-        });
-        // sort our array
-        var sortedValues = dataValues.sort(function (a, b) {
-            return b - a;
-        });
-
-        // round the highest number and use as our large circle diameter
-        var maxValue = Math.round(sortedValues[0] / 1000) * 1000;
-
-        // calc the diameters
-        var largeDiameter = calcRadius(maxValue) * 2,
-            smallDiameter = largeDiameter / 2;
-
-        // select our circles container and set the height
-        $(".legend-circles").css('height', largeDiameter.toFixed());
-
-        // set width and height for large circle
-        $('.legend-large').css({
-            'width': largeDiameter.toFixed(),
-            'height': largeDiameter.toFixed()
-        });
-        // set width and height for small circle and position
-        $('.legend-small').css({
-            'width': smallDiameter.toFixed(),
-            'height': smallDiameter.toFixed(),
-            'top': largeDiameter - smallDiameter,
-            'left': smallDiameter / 2
-        })
-
-        // label the max and median value
-        $(".legend-large-label").html(maxValue);
-        $(".legend-small-label").html((maxValue / 2));
-
-        // adjust the position of the large based on size of circle
-        $(".legend-large-label").css({
-            'top': -11,
-            'left': largeDiameter + 30,
-        });
-
-        // adjust the position of the large based on size of circle
-        $(".legend-small-label").css({
-            'top': smallDiameter - 11,
-            'left': largeDiameter + 30
-        });
-
-        // insert a couple hr elements and use to connect value label to top of each circle
-        $("<hr class='large'>").insertBefore(".legend-large-label")
-        $("<hr class='small'>").insertBefore(".legend-small-label").css('top', largeDiameter - smallDiameter - 8);
+        legend.addTo(map);
 
 
     }
-    //function to dynamically build info window
-    function retrieveInfo(boysLayer, currentYear) {
-        //shortcut to JQuery selection of the div with id=info
-        var info = $('#info');
-        //binds the mouseover to the boysLayer that displays and populates info window
-        boysLayer.on('mouseover', function (e) {
-            //removes the none class and shows the info window
-            info.removeClass('none').show();
-            //shortcut variable to e.layer.feature.properties
-            var props = e.layer.feature.properties;
-            //multiple JQuery calls to dynamically update the html of several divs in the info window
-            $('#info span').html(props.COUNTY);
-            //console.log(props.COUNTY);
-            $(".girls span:first-child").html('(Year ' + currentYear + ')');
-            $(".boys span:first-child").html('(Year ' + currentYear + ')');
-            $(".girls span:last-child").html(props['G' + currentYear]);
-            $(".boys span:last-child").html(props['B' + currentYear]);
-
-            // raise opacity level as visual affordance
-            e.layer.setStyle({
-                fillOpacity: .6
-            });
-
-            //empty arrays used to push values to be graphed with .sparkline
-            var girlsValues = [],
-                boysValues = [];
-
-            //for loop that populates the arrays upon mouseover
-            for (var i = 1; i <= 8; i++) {
-                girlsValues.push(props['G' + i]);
-                boysValues.push(props['B' + i]);
-            }
-
-            //creates the girlspark graph from the values pushed to the corresponding array and adds it to the div with class of girlspark
-            $('.girlspark').sparkline(girlsValues, {
-                width: '160px',
-                height: '30px',
-                lineColor: '#D96D02',
-                fillColor: '#d98939 ',
-                spotRadius: 0,
-                lineWidth: 2
-            });
-            //creates the boyspark graph from the values pushed to the corresponding array and adds it to the div with class of boyspark
-            $('.boyspark').sparkline(boysValues, {
-                width: '160px',
-                height: '30px',
-                lineColor: '#6E77B0',
-                fillColor: '#878db0',
-                spotRadius: 0,
-                lineWidth: 2
-            });
-
-        });
-        // hide the info panel when mousing off layergroup and remove affordance opacity
-        boysLayer.on('mouseout', function (e) {
-            info.hide();
-            e.layer.setStyle({
-                fillOpacity: 0
-            });
-        });
-
-        // when the mouse moves on the document
-        $(document).mousemove(function (e) {
-            // first offset from the mouse position of the info window
-            info.css({
-                "left": e.pageX + 6,
-                "top": e.pageY - info.height() - 25
-            });
-
-            // if it crashes into the top, flip it lower right
-            if (info.offset().top < 4) {
-                info.css({
-                    "top": e.pageY + 15
-                });
-            }
-            // if it crashes into the right, flip it to the left
-            if (info.offset().left + info.width() >= $(document).width() - 40) {
-                info.css({
-                    "left": e.pageX - info.width() - 80
-                });
-            }
-        });
-
-
-    }
-
 })();
