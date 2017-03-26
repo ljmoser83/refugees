1d3c2d5d95570e867a075fc838befc0328ad2592
Lee Moser <lee.moser@uky.edu>
2017-03-26 11:28:27 -0400

    close to complete

--- js/app.js
+++ js/app.js
@@ -3,59 +3,30 @@
     // mapbox access token for ljmoser83 account
     L.mapbox.accessToken = 'pk.eyJ1IjoibGptb3NlcjgzIiwiYSI6ImNqMG5sNmI1bjAwY3UzM3Q4cXNncGl6NDMifQ.jAX66dC8oy8Mh4IvgF5SPg';
 
-    // create the Leaflet map using mapbox.light tiles
+    // create the Leaflet map using mapbox.dark tiles. sets zoom levels to appropriate levels for the extent of dataLayer
     var map = L.mapbox.map('map', 'mapbox.dark', {
         zoomSnap: .1,
         center: [45, -106],
-        zoom: 4,
+        zoom: 9,
         minZoom: 4,
         maxZoom: 9,
     });
 
-    //AJAX request to retrieve data file from server hosted JSON
+    //AJAX request to retrieve data file from server hosted json file
     $.getJSON("data/refugees.json", function (data) {
 
-//        // loop through GeoJSON features
-//        data.features.forEach(function (state) {
-//
-//            // shortcut for props
-//            var props = state.properties;
-//            // for each property
-//            for (var prop in props) {
-//                // if the value is 0
-//                //was having trouble with the original push you sent removing 0 values from the total fields for a year for a state. Sure there is a better way to do this.
-//                if (props[prop] === props["Total_2008"]) {
-//                    continue;
-//                } else if (props[prop] === props["Total_2009"]) {
-//                    continue;
-//                } else if (props[prop] === props["Total_2010"]) {
-//                    continue;
-//                } else if (props[prop] === props["Total_2011"]) {
-//                    continue;
-//                } else if (props[prop] === props["Total_2012"]) {
-//                    continue;
-//                } else if (props[prop] === props["Total_2013"]) {
-//                    continue;
-//                } else if (props[prop] === props["Total_2014"]) {
-//                    continue;
-//                } else if (props[prop] === props["Total_2015"]) {
-//                    continue;
-//                } else if (props[prop] === props["Total_2016"]) {
-//                    continue;
-//                } else if (props[prop] == 0) {
-//                    // remove the property from the data
-//                    delete props[prop];
-//
-//                }
-//            }
-//        });
-
+        //call to drawLegend function that styles the legend and adds to the #map div
         drawLegend(data);
-        
+
+        //call to the drawMap function that creates the L.geoJson stored as the var= dataLayer. This function also applies additional styling with Leaflet methods and subsequent function calls to initiate user interaction funcitonality//
         drawMap(data);
+
+        //function styles and places the info window
         drawInfo();
 
     })
+
+    //Options to be used in the L.geoJson method call that converts points to layers, makes the circles with the L.circleMarker method, and styles the layers.
     var options = {
 
         pointToLayer: function (feature, ll) {
@@ -91,30 +62,6 @@
     // add the control to the map
     sliderControl.addTo(map);
 
-    // create Leaflet control for the ban country selector
-    var banSelect = L.control({
-        position: 'topleft'
-    });
-
-    // when added to the map
-    banSelect.onAdd = function (map) {
-
-        // select the element with id of 'ban'
-        var control = L.DomUtil.get("ban");
-
-        // disable the mouse events
-        L.DomEvent.disableScrollPropagation(control);
-        L.DomEvent.disableClickPropagation(control);
-
-        // add slider to the control
-        return control;
-
-    }
-
-    // add the control to the map
-    banSelect.addTo(map);
-
-
     // create Leaflet control for the year display
     var yearDisplay = L.control({
         position: 'bottomleft'
@@ -128,153 +75,100 @@
         return year;
     }
 
+    // add the control to the map
     yearDisplay.addTo(map);
 
+    //function to calculate the appropriate radius for the circleMarkers based on the total number of refugees for a layer for a given year.
     function calcRadius(val) {
         var radius = Math.sqrt(val / Math.PI);
         return radius
     }
 
+    //function that resizes the circleMarkers each time the slider UI is interacted with
     function resizeCircles(dataLayer, currentYear) {
         dataLayer.eachLayer(function (layer) {
             var radius = calcRadius(Number(layer.feature.properties['Total_' + currentYear]));
             layer.setRadius(radius);
         });
     }
+
     //function that dynamically styles the map initially
     function drawMap(data) {
+
+        //processes the data brought into script by AJAX request and creates and styles L.circleMarkers in the map div
         var dataLayer = L.geoJson(data, options).addTo(map);
-        map.zoomControl.setPosition('topright');
-       map.fitBounds(dataLayer.getBounds());
+
+        //gets the extent of dataLayer and applies as the bounds of the map
+        map.fitBounds(dataLayer.getBounds());
+
+        //backs the map's zoom out 1 level from the extent that was set with the fitBounds method above
         map.zoomOut(1);
+
         //creates a var named currentYear to set initial value of year identifier div upon webpage load
         var currentYear = 2016
 
-        $('.ban').on('mouseover', function () {
-            dataLayer.eachLayer(function (layer) {
-                layer.setStyle({
-                    fillOpacity: 0
-                });
-            });
-        });
-        //JQuery method to update html of selected div
+        //JQuery method to update html of the year div
         $('#year').html("Year:" + " " + currentYear);
-        
-                dataLayer.on('mouseover', function () {
-                    $(".info").show();
-                });
-                dataLayer.on('mouseout', function () {
-                    $(".info").hide();
-                });
-        
-        dataLayer.eachLayer(function (layer) {
-                layer.on('mouseover', function (layer) {
-                    updateInfo(this, currentYear);
-                    //                var selectedLayer = event.layer;
-                    //                var prop = selectedLayer.feature.properties;
-                    //from initial method of creating info panel
-                    this.setStyle({
-                        color: "yellow"
 
-                    }).bringToFront();
-                    updateInfo(this, currentYear);
+        //eventListeneer added on dataLayer that shows the info window on mouseover
+        dataLayer.on('mouseover', function () {
+            $(".info").show();
+        });
 
-                   
-                    dataLayer.on("mouseout", function (event) {
-                        //                    var selectedLayer = event.layer;
-                        // from initial solo creation
-                        this.setStyle({
-                            color: 'blue'
-                        });
-                        //creates an event listener that reverts the color of a layer's path stroke to the original value
-                        
-                    });
-                });
-            });
-       
+        //eventListeneer added on dataLayer that hides the info window on mouseout
+        dataLayer.on('mouseout', function () {
+            $(".info").hide();
+        });
+
+        //sets style of layers when update Map is called and resizes circles based on the total number of refugees by currentYear
         updateMap(dataLayer, currentYear);
-        //a call of the updateMap function that fires everytime the dropdown menu is selected. Within this function call, the legend content is dynamically generated each time the drop down menu is toggled by the user.
-
-        //call to retrieveInfo function that is used to build info window
-        //retrieveInfo(dataLayer, currentYear);
-        //call to sequenceUI function that established slider interaction
-        sequenceUI(dataLayer);
-        //        resetBan(dataLayer, currentYear)
-        banUI(dataLayer, currentYear);
     }
     //function that will update data by year and state and update symbology upon user interaction
     function updateMap(dataLayer, currentYear) {
 
+        //calls resizeCircles function
         resizeCircles(dataLayer, currentYear);
-        dataLayer.eachLayer(function (layer) {
 
-            layer.setStyle({
-                color: 'blue',
-                fillOpacity: 0
-            });
-        });
-//        dataLayer.eachLayer(function (layer) {
-//            layer.bindPopup('Name: ' + layer.feature.properties.NAME + '<br/>' + "Total Refugees "+currentYear+": " + layer.feature.properties['Total_' + currentYear], {
-//                maxWidth: 800
-//            }).openPopup;
-//        })
+        //call to sequenceUI that adds a listener to the slider UI that updates currentYear 
+        sequenceUI(dataLayer);
 
+        //creates initial content for the info window
+        layerInfo(dataLayer, currentYear);
 
     } //end updateMap function
-
-
 
 
     //adds an event listener that updates the value of currentYear each time the user interacts with the slider. This function also uses a JQuery method to update the content of the year label div.//
     function sequenceUI(dataLayer) {
         $('.slider').on('input change', function () {
             var currentYear = $(this).val();
-            map.closePopup();
             $('#year').html("Year:" + " " + currentYear);
-            //retrieveInfo(dataLayer, currentYear);
-            updateMap(dataLayer, currentYear)
-            $('.ban').val('all');
+            layerInfo(dataLayer, currentYear);
+            updateMap(dataLayer, currentYear);
         });
 
     }
 
-    function banUI(dataLayer, currentYear) {
-        $('.ban').on('input', function () {
-            var banned = $(this).val();
-            map.closePopup();
+    //drawInfo function taht creates and initially hides info window
+    function drawInfo() {
 
-            console.log(banned + currentYear.toString());
-            dataLayer.eachLayer(function (layer) {
-                if (layer.feature.properties[banned + currentYear] > 0) {
-                    layer.setStyle({
-                        fillOpacity: 0.25
-                    });
-                }
-            });
-            //retrieveInfo(dataLayer, banned, currentYear);
-            
-
+        var info = L.control({
+            position: 'topright'
         });
-    }
 
-        function drawInfo() {
-    
-            var info = L.control({
-                position: 'topright'
-            });
-    
-            info.onAdd = function (map) {
-    
-                var div = L.DomUtil.create('div', 'info');
-    
-                return div;
-    
-            }
-            
-            info.addTo(map);
-            $(".info").hide();
+        info.onAdd = function (map) {
+
+            var div = L.DomUtil.create('div', 'info');
+
+            return div;
+
         }
 
+        info.addTo(map);
+        $(".info").hide();
+    }
+
+    //function that draws legend based on max value to the dataset
     function drawLegend(data) {
         // create Leaflet control for the legend
         var legend = L.control({
@@ -283,21 +177,21 @@
         // when added to the map
         legend.onAdd = function (map) {
 
-                // select the element with id of 'legend'
-                var div = L.DomUtil.get("legend");
+            // select the element with id of 'legend'
+            var div = L.DomUtil.get("legend");
 
-                // disable the mouse events
-                L.DomEvent.disableScrollPropagation(div);
-                L.DomEvent.disableClickPropagation(div);
+            // disable the mouse events
+            L.DomEvent.disableScrollPropagation(div);
+            L.DomEvent.disableClickPropagation(div);
 
-                // add legend to the control
-                return div;
+            // add legend to the control
+            return div;
 
-            }
-            // add the control to the map
-            //Create an empty array named dataValues.
+        }
+
+        //Create an empty array named dataValues.
         var dataValues = [];
-        //Use the map method to iterate through the data.features.school.properties.grade enumerable enrollment values and push all numeric values to the array to sort for maximum of the enrollment rates.//
+        //Use the map method to iterate through the data.features.state.properties[total] enumerable refugee totals and push all numeric values to the array to sort for maximum of the enrollment rates.//
 
         data.features.map(function (state) {
 
@@ -364,21 +258,41 @@
 
         legend.addTo(map);
     }
+
+    //updates the info in the information window
     function updateInfo(layer, currentYear) {
 
-            var props = layer.feature.properties;
+        var props = layer.feature.properties;
 
-            var html = "<h3>" + props['NAME'] + "</h3>" +
-                "Syrian Refugees "+currentYear+ ": <b>" + props["Syria_"+currentYear] + "</b><br>" +
-                "Iranian Refugees "+currentYear+ ": <b>" + props["Iran_"+currentYear] + "</b><br>" +
-                "Yemeni Refugees "+currentYear+ ": <b>" + props["Yemen_"+currentYear] + "</b><br>" +
-                "Sudanese Refugees "+currentYear+ ": <b>" + props["Sudan_"+currentYear] + "</b><br>" +
-                "Libyan Refugees "+currentYear+ ": <b>" + props["Libya_"+currentYear] + "</b><br>" +
-                "Somalian Refugees "+currentYear+ ": <b>" + props["Somalia_"+currentYear] + "</b><br>" +
-                "Total Refugees: <b>" + props["Total_"+currentYear] + "</b>"
+        var html = "<h3>" + props['NAME'] + "</h3>" +
+            "Syrian Refugees " + currentYear + ": <b>" + props["Syria_" + currentYear] + "</b><br>" +
+            "Iranian Refugees " + currentYear + ": <b>" + props["Iran_" + currentYear] + "</b><br>" +
+            "Yemeni Refugees " + currentYear + ": <b>" + props["Yemen_" + currentYear] + "</b><br>" +
+            "Sudanese Refugees " + currentYear + ": <b>" + props["Sudan_" + currentYear] + "</b><br>" +
+            "Libyan Refugees " + currentYear + ": <b>" + props["Libya_" + currentYear] + "</b><br>" +
+            "Somalian Refugees " + currentYear + ": <b>" + props["Somalia_" + currentYear] + "</b><br>" +
+            "Total Refugees " + currentYear + ": <b>" + props["Total_" + currentYear] + "</b>"
 
-            $(".info").html(html);
-        }
-        //this function calculates the info pane content and adds it to the panel upon mouseover of each county
+        $(".info").html(html);
+    }
+
+    //this function calculates the info pane content and adds it to the panel upon mouseover of each county also adds mouseover affordance changing outline to yellow
+    function layerInfo(dataLayer, currentYear) {
+        dataLayer.eachLayer(function (layer) {
+            layer.on('mouseover', function (layer) {
+                updateInfo(this, currentYear);
+                this.setStyle({
+                    color: "yellow"
+                });
+            });
+        });
+        dataLayer.on("mouseout", function (event) {
+
+            this.setStyle({
+                color: 'blue'
+            });
+        });
+    }
+
 
 })();
